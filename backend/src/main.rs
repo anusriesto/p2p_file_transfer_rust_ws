d959615af281fc1855fb8adee2dbe7549df4bab1
anusriesto <anujwrap@live.com>
2025-04-22 18:07:42 +0530

    update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,122 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, sync::{mpsc, Arc}, time::Duration};
+use axum::{extract::{ws::{Message, WebSocket}, State, WebSocketUpgrade}, response::IntoResponse, routing::get, Router};
+use serde_json::Value;
+use tokio::{sync::broadcast, time::interval};
+
+use futures::{lock::Mutex, SinkExt, StreamExt};
+use uuid::Uuid;
+
+#[derive(Clone)]
+struct AppState{
+    connections:Arc<Mutex<HashMap<String,broadcast::Sender<Message>>>>, 
+}
+#[tokio::main]
+async fn main() {
+    let state=AppState{
+        connections:Arc::new(Mutex::new(HashMap::new()))
+    };
+    let app= Router::new()
+    .route("/ws",get(websocket_handler))
+    .with_state(state);
+    let listner=tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("Server running on http://localhost:8000");
+    axum::serve(listner, app).await.unwrap();
+
+}
+
+
+async fn websocket_handler(
+    ws:WebSocketUpgrade,
+    State(state):State<AppState>,
+)->impl IntoResponse{
+    ws.on_upgrade(move|socket|handle_socket(socket, state))
+}
+
+async fn handle_socket(socket:WebSocket,state:AppState){
+    let conn_id=Uuid::new_v4().to_string();
+    let conn_id_clone=conn_id.clone();
+    println!("New Connection:{}",conn_id);
+    let(tx ,mut rx)=broadcast::channel(100);
+    {
+        let mut connections=state.connections.lock().await;
+        connections.insert(conn_id.clone(),tx.clone());
+    }
+    let (mut sender,mut receiver)=socket.split();
+    let (message_tx,mut message_rx)=mpsc::channel::<Message>; // yahan buffer 100 tha karke
+    let sender_task=tokio::spawn(async move{
+        while let Some(msg)=message_rx.recv().await{
+            if sender.send(msg).await.is_err(){
+                break;
+            }
+        }
+    });
+
+    let ping_tx=message_tx.clone();
+    let ping_task=tokio::spawn(async move{
+        let mut interval=interval(Duration::from_secs(30));
+        loop{
+            interval.tick().await;
+            if ping_tx.send(Message::Ping(vec![])).await.is_err(){
+                break;
+            }
+        }
+    });
+
+    let forwar_tx=message_tx.clone();
+    let forward_task=tokio::spawn(async move{
+        while let Ok(msg)=rx.recv().await{
+            if forwar_tx.send(msg).await.is_err(){
+                break;
+            }
+        }
+    });
+    let receive_task=tokio::spawn(async move{
+        let state =state.clone();
+        let tx=tx.clone();
+        let mut target_map=HashMap::new();
+
+        async move{
+            while let Some(Ok(msg))=receiver.next().await{
+                match msg {
+                    Message::Text(text)=>{
+                        if let Ok(data)=serde_json::from_str::<Value>(&text){
+                            if data["type"]=="register"{
+                                if let Some(id)=data["connectionId"].as_str(){
+                                    state.connections.lock().await.insert(id.to_string(),tx.clone());
+                                }
+                                continue;
+                            }
+                            if let Some(target_id)=data["target_id"].as_str(){
+                                target_map.insert(conn_id.clone(), target_id.to_string());
+                                if let Some(target_tx)=state.connections.lock().await.get(target_id){
+                                    let _= target_tx.send(Message::Text((text)));
+                                }
+                            }
+                        }
+                    }
+                    Message::Binary(bin_data)=>{
+                        if let Some(target_id)=target_map.get(&conn_id){
+                            if let Some(target_tx)=state.connections.lock().await.get(target_id){
+                                let _=target_tx.send(Message::Binary((bin_data)));
+                            }
+                        }
+                        else {
+                            println!("No target set for binary transfer from {}",conn_id);
+                        }
+                    }
+                    Message::Close(_)=>break,
+                    _=>continue,
+                }
+            }
+        }
+
+    });
+    tokio::select! {
+        _=sender_task=>{},
+        _=ping_task=>{},
+        _=forward_task=>{},
+        _=receive_task=>{},
+    };
+    state.connections.lock().await.remove(&conn_id_clone);
+    println!("Connection closed{}",conn_id_clone)
 }
